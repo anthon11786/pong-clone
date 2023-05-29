f0d176ac481a3e9db9bd53fc218e946b180730bb
anthon11786 <anthonytperaza@gmail.com>
2023-05-28 23:13:07 -0400

    Fix collide and score bug and add HTML finishing touches

--- main.js
+++ main.js
@@ -21,13 +21,13 @@ class Rect {
         this.size = new Vec(w, h);
     }
     get left() {
-        return this.pos.x; - this.size.x /2; 
+        return this.pos.x - this.size.x /2; 
     }
     get right() { 
         return this.pos.x + this.size.x / 2; 
     }
     get top() {
-        return this.pos.y; - this.size.y /2; 
+        return this.pos.y - this.size.y /2; 
     }
     get bottom() {
         return this.pos.y + this.size.y / 2; 
@@ -86,6 +86,41 @@ class Pong {
         };
         this.reset();
 
+        // Pixel definitions for score numbers 
+        this.CHAR_PIXEL = 10; 
+        this.CHARS = [
+            '111101101101111',
+            '010010010010010',
+            '111001111100111',
+            '111001111001111',
+            '101101111001001',
+            '111100111001111',
+            '111100111101111',
+            '111001001001001',
+            '111101111101111',
+            '111101111001111',
+        ].map(str => {
+            // Creates an array of canvases we can paint later 
+            // For each string we create a new canvas, for each '1' we create white square 
+            const canvas = document.createElement('canvas'); // create a new canvas for score
+            canvas.width = this.CHAR_PIXEL * 3;
+            canvas.height = this.CHAR_PIXEL * 5;
+
+            const context = canvas.getContext("2d"); // create a new canvas for score
+            context.fillStyle = '#fff'; // fill it with white
+
+            str.split('').forEach((fill, i) => {
+                // if its a '1' we paint context 
+                if (fill === '1') {
+                    context.fillRect(
+                        (i % 3 ) * this.CHAR_PIXEL, 
+                        (i / 3 | 0 ) * this.CHAR_PIXEL, 
+                        this.CHAR_PIXEL, 
+                        this.CHAR_PIXEL);
+                }
+            });
+            return canvas;
+        })
         callback();
 
         
@@ -115,7 +150,22 @@ class Pong {
         this.players.forEach(player => {
             this.drawRect(player);
         })
+        this.drawScore();
 
+    }
+    drawScore() {
+        const align = this._canvas.width / 3; 
+        const CHAR_W = this.CHAR_PIXEL * 4; 
+        this.players.forEach((player,  index) => {
+            const chars = player.score.toString().split('');
+            // Calculate offset from left 
+            const offset = align * 
+                            (index + 1) - 
+                            (CHAR_W * chars.length / 2) + this.CHAR_PIXEL /2; 
+            chars.forEach((char, pos) => {
+                this._context.drawImage(this.CHARS[char|0], offset + pos * CHAR_W, 20)
+            })
+        })
     }
     drawRect(rect) {
         // white square 
@@ -152,10 +202,10 @@ class Pong {
         if (this.ball.left < 0 || this.ball.right > this._canvas.width) {
             this.ball.vel.x = -this.ball.vel.x;
             if (this.ball.vel.x > 0) {
-                this.players[0].score += 1;
+                this.players[1].score += 1;
             }
             else {
-                this.players[1].score += 1; 
+                this.players[0].score += 1; 
             }   
             this.players.forEach(player => {
                 console.log(player.score);
